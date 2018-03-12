0cfb2c2d9b10a49d61dacb88bd053921a699014e
starpentagon <starpentagon@gmail.com>
2018-03-13 00:52:58 +0900

    Add summarize_error_log.py

---
+++ summarize_error_log.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import sys
+import pandas as pd
+
+args = sys.argv
+
+if len(args) != 2:
+    print('Usage: ./summarize_error_log (error_log)')
+
+error_log_file = args[1]
+
+error_log_df = pd.read_csv(error_log_file)
+
+col = 'reason'
+reason_df = error_log_df.groupby(col)[col].count()
+print(reason_df)
+
+col = 'same/diff_result'
+modified_df = error_log_df.groupby(col)[col].count()
+print(modified_df)
